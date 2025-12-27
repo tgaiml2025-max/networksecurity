ad0044f3941c6bb3bf59f96454f5cc3b3a6c80d8
tgaiml2025-max <tgaiml2025@gmail.com>
2025-12-27 12:33:05 +0530

    MLFlow setup Completed

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngetionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
+
+from networksecurity.component.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
+
 if __name__=='__main__':
     try:
         trainingpipelineconfig = TrainingPipelineConfig()
@@ -36,8 +40,12 @@ if __name__=='__main__':
         print(data_transformation_artifact)
         logging.info("Data Transformation Completed")
         
-        
-        
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
         
         
     except Exception as e:
